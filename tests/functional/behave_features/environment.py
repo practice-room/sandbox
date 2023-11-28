ecf54cdf218d5a73d448e847ca267ea226423ddc
komish <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 21:21:09 +0000

    CI TESTING MODS

--- tests/functional/behave_features/environment.py
+++ tests/functional/behave_features/environment.py
@@ -2,19 +2,24 @@ from behave import fixture, use_fixture
 from common.utils.chart_certification import ChartCertificationE2ETestSingle
 from common.utils.chart_certification import ChartCertificationE2ETestMultiple
 
+import logging
 
 @fixture
 def workflow_test(context):
     context.workflow_test = ChartCertificationE2ETestSingle(test_name=context.test_name)
     yield context.workflow_test
+    logging.debug("starting cleanup of workflow_test") 
     context.workflow_test.cleanup()
+    logging.debug("ending cleanup of workflow_test")
 
 
 @fixture
 def submitted_chart_test(context):
     context.chart_test = ChartCertificationE2ETestMultiple()
     yield context.chart_test
+    logging.debug("starting cleanup of submitted_chart_test") 
     context.chart_test.cleanup()
+    logging.debug("starting cleanup of submitted_chart_test") 
 
 
 def before_scenario(context, scenario):
@@ -24,3 +29,4 @@ def before_scenario(context, scenario):
     else:
         context.test_name = scenario.name.split("@")[0][:-4].split("]")[1]
         use_fixture(workflow_test, context)
+        print("made it")

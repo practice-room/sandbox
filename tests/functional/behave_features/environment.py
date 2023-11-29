18e0a5a26317f8c402cb4277099fefe1c7e50777
komish <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 16:16:25 +0000

    add conditional statement to cleanup

--- tests/functional/behave_features/environment.py
+++ tests/functional/behave_features/environment.py
@@ -3,23 +3,26 @@ from common.utils.chart_certification import ChartCertificationE2ETestSingle
 from common.utils.chart_certification import ChartCertificationE2ETestMultiple
 
 import logging
+import os
 
 @fixture
 def workflow_test(context):
     context.workflow_test = ChartCertificationE2ETestSingle(test_name=context.test_name)
     yield context.workflow_test
-    logging.debug("starting cleanup of workflow_test") 
-    context.workflow_test.cleanup()
-    logging.debug("ending cleanup of workflow_test")
+    if os.getenv("SKIP_CLEANUP", False) is not True:
+        logging.debug("starting cleanup of workflow_test") 
+        context.workflow_test.cleanup()
+        logging.debug("ending cleanup of workflow_test")
 
 
 @fixture
 def submitted_chart_test(context):
     context.chart_test = ChartCertificationE2ETestMultiple()
     yield context.chart_test
-    logging.debug("starting cleanup of submitted_chart_test") 
-    context.chart_test.cleanup()
-    logging.debug("starting cleanup of submitted_chart_test") 
+    if os.getenv("SKIP_CLEANUP", False) is not True:
+        logging.debug("starting cleanup of submitted_chart_test") 
+        context.chart_test.cleanup()
+        logging.debug("starting cleanup of submitted_chart_test") 
 
 
 def before_scenario(context, scenario):

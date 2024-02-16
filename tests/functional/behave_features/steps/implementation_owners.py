c3caa9f61bfc132e00beeefc3d29f1de0a5fe942
Jose R. Gonzalez <komish@flutes.dev>
2024-02-16 11:26:58 -0600

    Use the right create func
    Signed-off-by: Jose R. Gonzalez <komish@flutes.dev>

--- tests/functional/behave_features/steps/implementation_owners.py
+++ tests/functional/behave_features/steps/implementation_owners.py
@@ -14,7 +14,7 @@ def impl_label_and_name(context, vendor_label: str, vendor_name: str):
     context.owners_file_test.vendor_label = vendor_label
     context.owners_file_test.vendor_name = vendor_name
     context.owners_file_test.vendor_category = 'redhat'
-    context.owners_file_test.create_and_commit_redhat_owners_file()
+    context.owners_file_test.create_and_commit_owners_file()
     context.owners_file_test.send_pull_request()
 
 @then('validation CI should conclude with result "{result}"')
